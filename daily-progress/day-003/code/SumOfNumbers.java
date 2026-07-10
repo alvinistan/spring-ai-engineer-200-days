8c790fd31c1b94ca092b1684b9f992d9b54adcb7
alvinistan <alvinistan@students.nsbm.ac.lk>
2026-07-10 17:31:06 +0530

    sum of user defined numbers with limit, Multiplication Table Generator

---
+++ daily-progress/day-003/code/SumOfNumbers.java
@@ -0,0 +1,30 @@
+package code;
+
+import java.util.Scanner;
+
+    public class SumOfNumbers {
+        static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter a positive limit: ");
+            int limit = scanner.nextInt();
+
+            if (limit < 1) {
+                System.out.println("The limit must be positive.");
+            } else {
+
+                int sum = 0;
+
+                for (int number = 1; number <= limit; number++) {
+                    sum = sum + number;
+                }
+
+                System.out.println("Sum from 1 to " + limit + " = " + sum);
+            }
+
+            scanner.close();
+        }
+    }
+
+
