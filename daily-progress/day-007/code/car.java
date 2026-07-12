bf960df97c0f5452dd56b4c13b1f049847887ad5
alvinistan <alvinistan@students.nsbm.ac.lk>
2026-07-12 20:53:02 +0530

    Objects accessing

---
+++ daily-progress/day-007/code/car.java
@@ -0,0 +1,21 @@
+package code;
+
+import java.time.Year;
+
+public class car {
+
+    String brand;
+    String model;
+    int year;
+
+    void start(){
+        System.out.println("Car started");
+        System.out.println("-------------------");
+        System.out.println("Car Brand : " + brand);
+        System.out.println("Car Model : " + model);
+        System.out.println("Car Year : " + year);
+        System.out.println("-------------------");
+    }
+
+}
+
