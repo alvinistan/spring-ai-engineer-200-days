90e1f4470523ca1604912b2fdf431aee2cac1915
alvinistan <alvinistan@students.nsbm.ac.lk>
2026-07-13 20:25:40 +0530

    Students Constructors

---
+++ daily-progress/day-008/code/student.java
@@ -0,0 +1,29 @@
+package code;
+public class student {
+
+    int studentId;
+    String studentName;
+
+    student(){
+        System.out.println("Student object created...");
+    }
+
+    student(int studentId, String studentName){
+        this.studentId = studentId;
+        this.studentName = studentName;
+
+    }
+    void display(){
+
+        System.out.println("Student ID: " +studentId);
+        System.out.println("Student Name: "+studentName);
+    }
+
+    static void main(String[] args) {
+        student s1 = new student();
+        student s2 = new student(101,"Alkanistan");
+
+        s2.display();
+        s1.display();
+    }
+}
