8caee3ad268f0d3e39bc65c164bdbfeaba85b6c5
alvinistan <alvinistan@students.nsbm.ac.lk>
2026-07-18 20:31:23 +0530

    Thread with Synchronization

--- daily-progress/day-019/code/ThreadMethodDemo.java
+++ daily-progress/day-019/code/ThreadMethodDemo.java
@@ -2,10 +2,17 @@ package code;
 
 public class ThreadMethodDemo {
     static void main(String[] args) {
-        Thread thread = new Thread(()-> {
-            System.out.println(Thread.currentThread().getName());
-        });
-        thread.setName("Worker-1");
-        thread.start();
+       try{
+           Thread thread = new Thread(() -> {
+               System.out.println(Thread.currentThread().getName());
+           });
+
+           thread.setName("Worker-1");
+           thread.sleep(5000);
+           thread.start();
+
+       } catch (InterruptedException e){
+           e.printStackTrace();
+       }
     }
 }
